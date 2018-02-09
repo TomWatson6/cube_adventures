c51a7b8820d8a1a5a56fd67148db043a38291c9b
BuildTools <unconfigured@null.spigotmc.org>
2018-02-09 19:40:46 +0000

    Updating cube_adventures

---
+++ nclgl/Mesh.h
@@ -0,0 +1,33 @@
+# pragma once
+#include "OGLRenderer.h"
+
+enum MeshBuffer {
+	VERTEX_BUFFER, COLOUR_BUFFER, TEXTURE_BUFFER, INDEX_BUFFER, MAX_BUFFER
+	
+};
+class Mesh {
+public:
+	Mesh(void);
+	~Mesh(void);
+	virtual void Draw();
+	static Mesh * GenerateTriangle();
+	void SetTexture(GLuint tex) { texture = tex; }
+	GLuint GetTexture() { return texture; }
+	static Mesh * GenerateQuad();
+		
+protected:
+	void BufferData();
+	GLuint numIndices;
+	unsigned int * indices;
+
+	GLuint arrayObject;
+	GLuint bufferObject[MAX_BUFFER];
+	GLuint numVertices;
+	GLuint type;
+	Vector3 * vertices;
+	Vector4 * colours;
+	GLuint texture;
+	Vector2 * textureCoords;
+
+	
+};
